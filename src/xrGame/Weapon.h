1e270fd9b7f5ce15f05dc98731ff302a0068b203
Pukkukk <puk2kuk@gmail.com>
2026-09-17 13:36:57 +0300

    Revert "Backport `scope_alive_detector` parameter from X-Ray 1.6"
    This reverts commit f469c369037592bc4bb2c9a38164db7519394e51.

--- src/xrGame/Weapon.h
+++ src/xrGame/Weapon.h
@@ -22,7 +22,6 @@ class CSE_ALifeItemWeaponAmmo;
 class CWeaponMagazined;
 class CParticlesObject;
 class CUIWindow;
-class CBinocularsVision;
 
 class CWeapon : public CHudItemObject,
 				public CShootingObject
@@ -226,8 +225,6 @@ protected:
 		Fvector4		m_ReloadDof;
 
 		BOOL			m_bUseDynamicZoom;
-		shared_str		m_sUseBinocularVision;
-		CBinocularsVision* m_pVision;
 
 	} m_zoom_params;
 	
