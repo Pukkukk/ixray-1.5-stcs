381321fb687bb95f3f9683cb776bc312f6daab71
Pukkukk <puk2kuk@gmail.com>
2026-09-17 13:29:16 +0300

    Revert "Замена ошибок на более корректный вариант"
    This reverts commit f938f191ded99fd60c68a4ccb4e1d36cffb75551.

--- src/Layers/xrRender/ResourceManager_Resources.cpp
+++ src/Layers/xrRender/ResourceManager_Resources.cpp
@@ -342,7 +342,13 @@ SPS*	CResourceManager::_CreatePS			(LPCSTR name)
 		_RELEASE		(pErrorBuf);
 		pConstants		= NULL;
 
-		R_ASSERT2(SUCCEEDED(hr), "Your video card doesn't meet game requirements\n\nPixel Shaders v1.1 or higher required");
+		if (FAILED(hr))
+			Msg			("Can't compile shader %s",name);
+
+		CHECK_OR_EXIT		(
+			!FAILED(hr),
+			make_string("Your video card doesn't meet game requirements\n\nPixel Shaders v1.1 or higher required")
+		);
 		return			_ps;
 	}
 }
